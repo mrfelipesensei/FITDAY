9f3b84fb8531b54b7231b7d14f6f0a9a58c4560b
Felipe Greg <mrfelipesensei@gmail.com>
2025-04-14 16:41:38 -0300

    inicio de lógica registrar_treino

--- app/auth.py
+++ app/auth.py
@@ -39,3 +39,48 @@ def register_user():
         with open("users.json","w") as f:
             json.dump(users,f)
         print("Cadastro realizado com sucesso.")
+
+def registrar_treino():
+    with open("treinos.json","r") as f:
+        treinos = json.load(f)
+    
+    if not os.path.exists("treinos.json"):
+        with open("treinos.json","w") as f:
+            json.dump({},f)
+
+
+
+'''Função registrar_treino(usuario):
+    Tentar abrir o arquivo "treinos.json" para leitura
+        Se o arquivo existir:
+            Carregar os dados do JSON em uma variável chamada "treinos"
+        Se o arquivo não existir:
+            Criar uma variável "treinos" como dicionário vazio
+
+    Obter a data atual no formato "YYYY-MM-DD"
+
+    Solicitar ao usuário:
+        - Grupo muscular
+        - Nome do exercício
+        - Peso (converter para float)
+        - Repetições (converter para int)
+
+    Criar um dicionário com os dados do treino:
+        {
+            "data": data_atual,
+            "grupo_muscular": valor digitado,
+            "exercicio": valor digitado,
+            "peso": valor digitado,
+            "repeticoes": valor digitado
+        }
+
+    Se o nome de usuário estiver em "treinos":
+        Adicionar o novo treino à lista do usuário com .append()
+    Senão:
+        Criar uma nova lista com esse treino como primeiro item e associar ao usuário
+
+    Abrir o arquivo "treinos.json" para escrita
+        Salvar os dados atualizados com json.dump()
+
+    Exibir mensagem: "Treino registrado com sucesso!"
+'''
